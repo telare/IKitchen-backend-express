86d5579c1e44e63560f368ed0e934120b232ab83
tl <andrewzhubinskiy21@gmail.com>
2025-08-18 18:08:41 +0300

    feat(routes): add GET endpoint for a /recipes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes";
+import recipesRoutes from "./routes/recipesRoutes";
 import { errorMiddleware } from "@shared/utils/middlewares";
 
 export const app = express();
@@ -10,6 +11,7 @@ if (!PORT) {
 
 app.use(express.json());
 app.use("/api/v1", authRoutes);
+app.use("/api/v1/recipes", recipesRoutes);
 app.use(errorMiddleware);
 
 app.listen(3000, () => {
